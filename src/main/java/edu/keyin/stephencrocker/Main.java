f72932d20657a67f25f13476ea7d4d8bc435bbb1
SearchingSteve <155838264+SearchingSteve@users.noreply.github.com>
2025-01-15 17:30:12 -0330

    feat: Temp Above Avg complete
    Simple java program to calculate the the average temperature and the average count of temperatures higher than the average

--- src/main/java/edu/keyin/stephencrocker/Main.java
+++ src/main/java/edu/keyin/stephencrocker/Main.java
@@ -1,7 +1,29 @@
 package edu.keyin.stephencrocker;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the number of days ");
+        int numDays = scanner.nextInt();
+
+        double[] temps = new double[numDays];
+        double tempSum = 0;
+        for (int i = 0; i < numDays; i++){
+            System.out.println("Please enter the temperature for the day " + i+1 + ": ");
+            temps[i] = scanner.nextDouble();
+            tempSum += temps[i];
+        }
+        double tempAvg = tempSum/numDays;
+        System.out.println("The average temperature is " + tempAvg);
+
+        int aboveAvgCount = 0;
+        for (int i = 0; i < numDays; i++){
+            if (temps[i] > tempAvg){
+                aboveAvgCount++;
+            }
+        }
+        System.out.println("The count of temperatures higher than the average is " + aboveAvgCount);
     }
 }
